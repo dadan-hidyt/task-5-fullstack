43f9bbd2c7a5a96201e1ad827905ecd8b260b62a
dadan-hidyt <dadanhidyt@gmail.com>
2022-12-02 23:15:42 -0800

    dadan

--- task-5-restfull-api/app/Http/Controllers/API/Article.php
+++ task-5-restfull-api/app/Http/Controllers/API/Article.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\API;
 use App\Http\Controllers\Controller;
 use App\Models\Article as ModelsArticle;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class Article extends Controller
 {
@@ -12,7 +13,32 @@ class Article extends Controller
     public function index(){
         return response()->json(['status'=>true,'data'=>ModelsArticle::paginate(25)],200);
     }
- 
+    public function detail($id = null) {
+        if (is_null($id)){
+            return null;
+        }
+        //mendapatkan data berdasarkan id
+        $detail = DB::table('articles')->where('id',$id)->first();
+        if ($detail){
+            return response()->json([
+                'status' => true,
+                'data' => $detail,
+            ]);
+        }
+        return response()->json([
+            'status' => false,
+            'message' => 'Artikel Tidak di temukan',
+        ]);
+    }
+    public function delete($id = null){
+        if (is_null($id)){
+            return null;
+        }
+        if (DB::table('articles')->where('id',$id)->delete()) {
+            return response()->json('Data berhasil di hapus');
+        }
+        return response()->json('data gagal di hapus');
+    }
     public function store(Request $request){
         $data = $request->validate([
             'title' => 'required',
@@ -22,5 +48,21 @@ class Article extends Controller
         ]);
         ModelsArticle::create($data);
         return response()->json(['status'=>'true','message'=>'Article success inserted','data'=>$data]);
+    }
+    public function update(Request $request){
+    
+        $data = $request->validate([
+            'id'=> 'required',
+            'title' => 'required',
+            'content' => 'required',
+            'category_id' => 'required',
+            'user_id' => 'required',
+        ]);
+        $update = DB::table('articles')->where('id',$data['id'])->update($data);
+        if($update){
+            return response()->json(['status'=>'true','message'=>'Article success updated','data'=>$data]);
+        } else {
+            return response()->json(['status'=>'false','message'=>'Article failed updated']);
+        }
     }
 }
--- task-5-restfull-api/routes/api.php
+++ task-5-restfull-api/routes/api.php
@@ -5,21 +5,13 @@ use App\Http\Controllers\API\PassportController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
-
 Route::prefix('v1')->group(function(){
     Route::post('login',[PassportController::class,'login'])->name('login');
     Route::middleware('auth:api')->group(function(){
         Route::get('list-all',[Article::class,'index']);
         Route::post('create',[Article::class,'store']);
+        Route::get('detail/{id}',[Article::class,'detail']);
+        Route::get('delete/{id}',[Article::class,'delete']);
+        Route::put('update',[Article::class,'update']);
     });
 });
